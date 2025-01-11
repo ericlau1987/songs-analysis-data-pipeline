02b9e7e9783eb74959cc67cf14e5d5606a9aeabc
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2025-01-11 17:26:40 +1100

    comment out the iam main

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,45 +1,45 @@
-resource "aws_iam_role_policy" "s3_bucket_policy" {
-  name = "s3_bucket_creation_policy"
-  role = iam_role.name.id
+# resource "aws_iam_role_policy" "s3_bucket_policy" {
+#   name = "s3_bucket_creation_policy"
+#   role = iam_role.name.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:CreateBucket",
-          "s3:PutBucketPolicy",
-          "s3:PutBucketTagging"
-        ]
-        Resource = "arn:aws:s3:::songs-analysis-data-pipeline-bucket-*"
-      }
-    ]
-  })
-}
-
-# resource "aws_iam_role" "songs-analysis-data-pipeline-role" {
-#      name               = "songs-analysis-data-pipeline-role"
-#      assume_role_policy = jsonencode({
+#   policy = jsonencode({
 #     Version = "2012-10-17"
 #     Statement = [
 #       {
-#         Action = "sts:AssumeRole"
 #         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
+#         Action = [
+#           "s3:CreateBucket",
+#           "s3:PutBucketPolicy",
+#           "s3:PutBucketTagging"
+#         ]
+#         Resource = "arn:aws:s3:::songs-analysis-data-pipeline-bucket-*"
+#       }
 #     ]
 #   })
 # }
 
-data "aws_iam_role" "songs-analysis-data-pipeline-role" {
+# # resource "aws_iam_role" "songs-analysis-data-pipeline-role" {
+# #      name               = "songs-analysis-data-pipeline-role"
+# #      assume_role_policy = jsonencode({
+# #     Version = "2012-10-17"
+# #     Statement = [
+# #       {
+# #         Action = "sts:AssumeRole"
+# #         Effect = "Allow"
+# #         Sid    = ""
+# #         Principal = {
+# #           Service = "ec2.amazonaws.com"
+# #         }
+# #       },
+# #     ]
+# #   })
+# # }
 
-}
+# data "aws_iam_role" "songs-analysis-data-pipeline-role" {
+#     arn = ""
+# }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role      = aws_iam_role.songs-analysis-data-pipeline-role.name
-  policy_arn = aws_iam_role_policy.s3_bucket_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "test-attach" {
+#   role      = aws_iam_role.songs-analysis-data-pipeline-role.name
+#   policy_arn = aws_iam_role_policy.s3_bucket_policy.arn
+# }
